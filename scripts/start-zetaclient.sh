e9f8f5c309664fc695ef515f5171e0079ed66ff0
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-09-15 13:55:22 -0400

    modify chain id to zetachain_7000-1

--- scripts/start-zetaclient.sh
+++ scripts/start-zetaclient.sh
@@ -35,6 +35,5 @@ peer=/ip4/"$SEEDIP"/tcp/6668/p2p/"$SEED"
 operatorAddress=$(zetacored keys show operator -a --keyring-backend=test)
 echo "operatorAddress: $operatorAddress"
 
-rm ~/.tss/*
 zetaclientd init --peer "$peer" --operator "$operatorAddress" --public-ip "$MYIP"
 zetaclientd start >> ~/.zetacored/zetaclient.log 2>&1  &
