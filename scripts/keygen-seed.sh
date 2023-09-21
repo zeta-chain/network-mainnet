89fe1219da77ca17ff18de55a9d27e45b8157379
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-09-21 14:30:58 -0400

    add genesis.json

--- scripts/keygen-seed.sh
+++ scripts/keygen-seed.sh
@@ -28,5 +28,5 @@ MYIP=$(curl ifconfig.me)
 operatorAddress=$(zetacored keys show operator -a --keyring-backend=test)
 echo "operatorAddress: $operatorAddress"
 
-zetaclientd init --operator "$operatorAddress" --public-ip "$MYIP" --chain-id "$CHAINID"
+zetaclientd init --operator "$operatorAddress" --public-ip "$MYIP"
 zetaclientd start >> ~/.zetacored/zetaclient.log 2>&1  &
