99a083a9b391923d8d4f640b41429617576e9401
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-09-21 00:10:00 -0400

    add genesis files

--- scripts/start-zetacore-clean.sh
+++ scripts/start-zetacore-clean.sh
@@ -20,4 +20,3 @@ rm -rf ~/.zetacored/config/genesis.json
 cp -a network_files/config/genesis.json ~/.zetacored/config/
 echo "Start zetacored---------->"
 zetacored start --pruning=nothing --minimum-gas-prices=0.0001azeta --json-rpc.api eth,txpool,personal,net,debug,web3,miner --api.enable >> ~/.zetacored/zetacored.log 2>&1  &
-tail -f ~/.zetacored/zetacored.log
