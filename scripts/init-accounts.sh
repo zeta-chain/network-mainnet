6a2399a6a0662eefee41e814193d78eb2f44e9eb
lumtis <lucas.bertrand.22@gmail.com>
2023-10-17 10:23:27 -0700

    update infstone address

--- scripts/init-accounts.sh
+++ scripts/init-accounts.sh
@@ -8,7 +8,7 @@ accounts=(
     "zeta1hjct6q7npsspsg3dgvzk3sdf89spmlpf7rqmnw" # Figment
     "zeta1p0uwsq4naus5r4l7l744upy0k8ezzj84mn40nf" # Bastion
     "zeta1t5pgk2fucx3drkynzew9zln5z9r7s3wqqyy0pe" # Blockdaemon
-    "zeta1zvg2f7dq5d528d9qfkpqcf444adnez9u8d520z" # InfStones
+    "zeta1t0uj2z93jd2g3w94zl3jhfrn2ek6dnuk3v93j9" # InfStones
 )
 
 # add genesis accounts
