0b6c7241c1f70c7ac2db474e4b399afe5abfddd2
lumtis <lucas.bertrand.22@gmail.com>
2023-10-18 13:23:35 -0700

    update accounts

--- scripts/init-accounts.sh
+++ scripts/init-accounts.sh
@@ -9,6 +9,8 @@ accounts=(
     "zeta1p0uwsq4naus5r4l7l744upy0k8ezzj84mn40nf" # Bastion
     "zeta1t5pgk2fucx3drkynzew9zln5z9r7s3wqqyy0pe" # Blockdaemon
     "zeta1t0uj2z93jd2g3w94zl3jhfrn2ek6dnuk3v93j9" # InfStones
+    "zeta1k6vh9y7ctn06pu5jngznv5dyy0rltl2qp0j30g" # Omnichain1
+    "zeta19jr7nl82lrktge35f52x9g5y5prmvchmk40zhg" # Omnichain2
 )
 
 # add genesis accounts
