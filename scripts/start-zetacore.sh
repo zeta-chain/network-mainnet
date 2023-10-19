af5774b8e07e327682f2e71558615cc724c68b8c
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-10-18 21:23:00 -0400

    add stop inbound

--- scripts/start-zetacore.sh
+++ scripts/start-zetacore.sh
@@ -7,4 +7,15 @@ HOSTNAME=$(hostname)
 
 rm -rf ~/.zetacored/config/genesis.json
 cp -a network_files/config/genesis.json ~/.zetacored/config/
-zetacored start --pruning=nothing --minimum-gas-prices=0.0001azeta --json-rpc.api eth,txpool,personal,net,debug,web3,miner --api.enable >> ~/.zetacored/zetacored.log 2>&1  &
+
+
+zetacored start \
+--minimum-gas-prices=0.0001azeta \
+--json-rpc.api eth,txpool,personal,net,debug,web3,miner \
+--api.enable \
+--pruning custom \
+--pruning-keep-recent 54000 \
+--pruning-interval 10 \
+--min-retain-blocks 54000 \
+--state-sync.snapshot-interval 10 \
+--state-sync.snapshot-keep-recent 5 >> ~/.zetacored/zetacored.log 2>&1  &
